bb0df30f502b4771aa2234a94f89adfe24bad547
Mark Leo <ratrateroo@gmail.com>
2019-08-30 22:53:55 +0800

    tried fetching data

--- app.js
+++ app.js
@@ -9,16 +9,19 @@ function searchWeather() {
     }
     var http = new XMLHttpRequest();
     var apiKey = '38b0adfe83945b38e59aa0d4ca87362a';
-    var url ='http://api.openweathermap.org/data/2.5/weather?q=' + cityName;
+    var url ='http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=metric&appid=' + apiKey;
     var method = 'GET';
 
     http.open(method, url);
     http.onreadystatechange = function() {
         if (http.readyState == XMLHttpRequest.DONE && http.status === 200) {
             var data = JSON.parse(http.responseText);
+            var weatherData = new Weather(cityName, data.weather[0].description.toUpperCase());
+            weatherData.temperature = data.main.temp;
+            console.log(weatherData);
         } else if (http.readyState === XMLHttpRequest.DONE) {
             alert('Something went wrong!');
         }
     };
-
+    http.send();
 }
