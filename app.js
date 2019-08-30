685dc58fe9db984ba1091b35bb2fb272ba940867
Mark Leo <ratrateroo@gmail.com>
2019-08-30 22:32:31 +0800

    set up xmlhttprequest

--- app.js
+++ app.js
@@ -3,5 +3,22 @@
 searchButton.addEventListener('click', searchWeather);
 
 function searchWeather() {
-    console.log(searchCity.value);
+    var cityName = searchCity.value;
+    if (cityName.trim().length == 0) {
+        return alert('Please enter a city name.');
+    }
+    var http = new XMLHttpRequest();
+    var apiKey = '38b0adfe83945b38e59aa0d4ca87362a';
+    var url ='http://api.openweathermap.org/data/2.5/weather?q=' + cityName;
+    var method = 'GET';
+
+    http.open(method, url);
+    http.onreadystatechange = function() {
+        if (http.readyState == XMLHttpRequest.DONE && http.status === 200) {
+
+        } else if (http.readyState === XMLHttpRequest.DONE) {
+            alert('Something went wrong!');
+        }
+    };
+    
 }
